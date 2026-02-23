d7bc57eeaaf752b4e55635d75693b9bda62fc344
Torben <krahnstoevert@wit.edu>
2026-02-23 16:03:59 -0500

    added attributes, getArrivalTimes method, getID method and JavaDoc comments

--- src/main/java/wit/transit/TrainArrival.java
+++ src/main/java/wit/transit/TrainArrival.java
@@ -1,4 +1,20 @@
 package wit.transit;
 
 public class TrainArrival {
+    DateTime[] arrival_times;
+    String id;
+
+    /**
+     * @return List arrival times in the form of a list of DateTime Objects
+     */
+    public DateTime[] getArrivalTimes() {
+        return arrival_times;
+    }
+
+    /**
+     * @return return the id as a String
+     */
+    public String getId(){
+        return id;
+    }
 }
