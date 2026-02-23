ca4cb76958b0d3899d0b91b085fc859a1b58cea9
Torben <krahnstoevert@wit.edu>
2026-02-23 16:04:44 -0500

    added javadoc comments added getID, getName, getLine methods

--- src/main/java/wit/transit/Trip.java
+++ src/main/java/wit/transit/Trip.java
@@ -1,7 +1,31 @@
 package wit.transit;
 
+/**
+ * in the MBTA API a single train completing its route one way is a Trip
+ */
 public class Trip {
     String id;
     String name;
     Line line;
+
+    /**
+     * @return id as a String
+     */
+    public String getId(){
+        return id;
+    }
+
+    /**
+     * @return name of the Trip as a String
+     */
+    public String getName(){
+        return name;
+    }
+
+    /**
+     * @return which line a trip is taking as a Line object
+     */
+    public Line getLine(){
+        return line;
+    }
 }
