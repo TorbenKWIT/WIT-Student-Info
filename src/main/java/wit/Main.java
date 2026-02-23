162202527e08b50c7c4cfa2c5d2678e2a188e71f
Torben <krahnstoevert@wit.edu>
2026-02-23 16:06:18 -0500

    added a constant var for the API-KEY

--- src/main/java/wit/Main.java
+++ src/main/java/wit/Main.java
@@ -6,9 +6,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        final String API_KEY = "";  // INSERT MBTA API KEY HERE
 
         Scanner scanner = new Scanner(System.in);
-        System.out.printf("Enter API Key: ");
+        System.out.printf("Enter API Key: "); // temp way to add the api key to prevent accidentally leaking apikeys
         String apiKey = scanner.nextLine();
 
         MBTA mbta = new MBTA();
