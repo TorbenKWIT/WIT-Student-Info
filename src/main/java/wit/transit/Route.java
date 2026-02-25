4c0e0f560691f62a5e8349adb7bff52b76ba7250
Torben <krahnstoevert@wit.edu>
2026-02-25 15:38:29 -0500

    added stops ArrayList attribute, updated Constructor, added addStopToRoute(), added getStopsOnRoute().

--- src/main/java/wit/transit/Route.java
+++ src/main/java/wit/transit/Route.java
@@ -1,17 +1,28 @@
 package wit.transit;
 
+import java.util.ArrayList;
+
 /**
  * a route is the
  */
 public class Route {
     String id;
     String name;
+    ArrayList<Stop> stops;
 
     Route(String id, String name){
         this.id=id;
         this.name=name;
+        this.stops= new ArrayList<>();
     }
 
+    protected void addStopToRoute(Stop stop){
+        stops.add(stop);
+    }
+
+    protected ArrayList<Stop> getStopsOnRoute(){
+        return stops;
+    }
     /**
      * @return string Id of the Route
      */
