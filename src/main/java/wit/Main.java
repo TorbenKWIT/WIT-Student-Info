513fc1240bd1849c59c9a9a41a6ffba80e5795a1
Torben <krahnstoevert@wit.edu>
2026-03-08 06:07:58 -0500

    Added the API_KEY global var

--- src/main/java/wit/Main.java
+++ src/main/java/wit/Main.java
@@ -1,26 +1,22 @@
 package wit;
 
-import wit.transit.MBTA;
-import java.util.Scanner;
+import wit.ui.UI;
+
+import javax.swing.*;
 
 public class Main {
+
+
+    /**
+     * BEFORE BUILDING THE PROJECT PLEASE ADD THE MBTA API KEY HERE
+     * MORE DETAILS ARE IN THE README.MD File
+     */
+    public static final String API_KEY = "";  // INSERT MBTA API KEY HERE
+
+
     public static void main(String[] args) {
-        final String API_KEY = "";  // INSERT MBTA API KEY HERE
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.printf("Enter API Key: "); // temp way to add the api key to prevent accidentally leaking apikeys
-        String apiKey = scanner.nextLine();
-
-        MBTA mbta = new MBTA();
-        mbta.setApiKey(apiKey);
-        mbta.pullData();
-
-
-
-
-
-
-        //System.out.println(mbta.getAllStops());
+        SwingUtilities.invokeLater(UI::new); // runs the UI
 
 
     }
