9da5726bed34767cd572a47e6f84388837f28671
Danny05-hub <dannynguyen2005@yahoo.com>
2026-03-08 13:12:27 -0400

    Update WebScraper.java

--- src/main/java/wit/webscrape/WebScraper.java
+++ src/main/java/wit/webscrape/WebScraper.java
@@ -1,79 +1,102 @@
-//package wit.webscrape;
-//
-////TODO
-//public class WebScraper {
-//import wit.transit.Train;
-//
-//import java.io.BufferedReader;
-//import java.io.InputStreamReader;
-//import java.net.HttpURLConnection;
-//import java.net.URL;
-//import java.util.ArrayList;
-//
-//import org.json.JSONArray;
-//import org.json.JSONObject;
-//
-//public class WebScraper {
-//
-//    private static final String API_URL = "https://api-v3.mbta.com/vehicles";
-//
-//    public static ArrayList<Train> getLiveTrains() {
-//
-//        ArrayList<Train> trains = new ArrayList<>();
-//
-//        try {
-//
-//            URL url = new URL(API_URL);
-//
-//            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-//
-//            conn.setRequestMethod("GET");
-//            conn.setRequestProperty("Accept", "application/json");
-//
-//            BufferedReader reader = new BufferedReader(
-//                    new InputStreamReader(conn.getInputStream())
-//            );
-//
-//            StringBuilder json = new StringBuilder();
-//            String line;
-//
-//            while ((line = reader.readLine()) != null) {
-//                json.append(line);
-//            }
-//
-//            reader.close();
-//
-//            JSONObject data = new JSONObject(json.toString());
-//
-//            JSONArray vehicles = data.getJSONArray("data");
-//
-//            for (int i = 0; i < vehicles.length(); i++) {
-//
-//                JSONObject vehicle = vehicles.getJSONObject(i);
-//                JSONObject attr = vehicle.getJSONObject("attributes");
-//
-//                if (attr.isNull("latitude") || attr.isNull("longitude"))
-//                    continue;
-//
-//                String id = vehicle.getString("id");
-//
-//                double lat = attr.getDouble("latitude");
-//                double lon = attr.getDouble("longitude");
-//
-//                String route = vehicle
-//                        .getJSONObject("relationships")
-//                        .getJSONObject("route")
-//                        .getJSONObject("data")
-//                        .getString("id");
-//
-//                trains.add(new Train(id, route, lat, lon));
-//            }
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//
-//        return trains;
-//    }
-//}
-//}
+package wit.webscrape;
+
+TODO
+public class WebScraper {
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class WebScraper {
+
+    public static void main(String[] args) {
+
+        scrapeLibrary();
+        scrapeDining();
+
+    }
+
+    // Scrape WIT Library page
+    public static void scrapeLibrary() {
+
+        try {
+
+            String url = "https://library.wit.edu/home";
+
+            Document doc = Jsoup.connect(url).get();
+
+            System.out.println("===== WIT LIBRARY =====");
+            System.out.println("Page Title: " + doc.title());
+
+            // Get headings
+            Elements headings = doc.select("h1, h2, h3");
+
+            System.out.println("\nHeadings:");
+
+            for (Element heading : headings) {
+                System.out.println(heading.text());
+            }
+
+            // Get links
+            Elements links = doc.select("a[href]");
+
+            System.out.println("\nLinks:");
+
+            for (Element link : links) {
+
+                String text = link.text();
+                String href = link.absUrl("href");
+
+                if (!text.isEmpty()) {
+                    System.out.println(text + " -> " + href);
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    // Scrape Campus Dining page
+    public static void scrapeDining() {
+
+        try {
+
+            String url = "https://wit.edu/about/campus-dining";
+
+            Document doc = Jsoup.connect(url).get();
+
+            System.out.println("\n===== CAMPUS DINING =====");
+            System.out.println("Page Title: " + doc.title());
+
+            // Get headings
+            Elements headings = doc.select("h1, h2, h3");
+
+            System.out.println("\nDining Sections:");
+
+            for (Element heading : headings) {
+                System.out.println(heading.text());
+            }
+
+            // Get paragraph info
+            Elements paragraphs = doc.select("p");
+
+            System.out.println("\nDining Information:");
+
+            for (Element p : paragraphs) {
+
+                String text = p.text();
+
+                if (text.length() > 50) {
+                    System.out.println(text);
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
